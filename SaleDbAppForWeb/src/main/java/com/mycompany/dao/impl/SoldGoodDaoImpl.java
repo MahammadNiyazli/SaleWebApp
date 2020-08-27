92256ce4a170191d7a6e4f39b028db32235e29f6
MahammadNiyazli <mahammad.niyazli@gmail.com>
2020-08-27 23:20:02 +0400

    changes

--- SaleDbAppForWeb/src/main/java/com/mycompany/dao/impl/SoldGoodDaoImpl.java
+++ SaleDbAppForWeb/src/main/java/com/mycompany/dao/impl/SoldGoodDaoImpl.java
@@ -1,34 +1,67 @@
 package com.mycompany.dao.impl;
 
+import com.mycompany.dao.AbstractDao;
 import com.mycompany.dao.inter.SoldGoodDaoInter;
 import com.mycompany.entity.SoldGood;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.Date;
 import java.util.List;
 
-public class SoldGoodDaoImpl implements SoldGoodDaoInter {
+public class SoldGoodDaoImpl extends AbstractDao implements SoldGoodDaoInter {
     @Override
     public List<SoldGood> getSoldGoodByUserId(int userId) {
-        return null;
+        EntityManager em = em();
+        Query query = em.createQuery("select sg from SoldGood sg where sg.userId=:userId");
+        query.setParameter("userId",userId);
+
+        List<SoldGood> list = query.getResultList();
+        em.close();
+        return list;
     }
 
     @Override
     public List<SoldGood> getSoldGoodByUserIdAndDate(int userId, Date date) {
-        return null;
+        EntityManager em = em();
+        Query query = em.createQuery("select sg from SoldGood sg where sg.userId=:userId and sg.salesDate=:salesDate");
+        query.setParameter("userId",userId);
+        query.setParameter("salesDate",date);
+
+        List<SoldGood> list = query.getResultList();
+        em.close();
+        return list;
     }
 
     @Override
     public boolean addSoldGood(SoldGood good) {
-        return false;
+        EntityManager em = em();
+        em.getTransaction().begin();
+        em.persist(good);
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
     @Override
     public boolean removeSoldGood(int id) {
-        return false;
+        EntityManager em = em();
+        SoldGood good = em.find(SoldGood.class,id);
+        em.getTransaction().begin();
+        em.remove(good);
+        em.getTransaction().commit();
+        em.close();
+        return true;
+
     }
 
     @Override
     public boolean updateSoldGood(SoldGood good) {
-        return false;
+        EntityManager em = em();
+        em.getTransaction().begin();
+        em.merge(good);
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 }
